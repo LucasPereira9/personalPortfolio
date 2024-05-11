9d1bcb79ffde122c534782c7d70b99de3ee1e384
LucasPereira9 <lucas970997@gmail.com>
2024-05-11 13:00:04 -0300

    feat: image part and cv download

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -2,6 +2,9 @@ import { firstComplement, secondComplement } from '@/utils';
 import styles from './about.module.css'
 import { IAboutProps } from './about.structure';
 import PrimaryButton from '../primaryButton';
+import Image from 'next/image';
+import AnimatedContainer from '../animatedContainer';
+import { AiOutlineCodepen   } from 'react-icons/ai';
 
 export default function About(props: IAboutProps) {
 
@@ -18,18 +21,37 @@ export default function About(props: IAboutProps) {
         </div>
     ));
 
+    const handleDownload = () => {
+        const downloadUrl = '/cvLucas.pdf';
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.setAttribute('download', 'cvLucas.pdf');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
+    
+
     return (
     <div id={props.id}>
         <div className={styles.content}>
-            <div>
-                <h2>image part</h2>
+            <div className={styles.icon_container}>
+                <AiOutlineCodepen  className={styles.icon}  />
+            </div>
+            <AnimatedContainer>
+                <Image width={720} height={680} src={'/assets/images/working.png'} alt={'working'} />
+            </AnimatedContainer>
+            <div className={styles.experience_container}>
+                <div className={styles.separator} />
+                <h1 className={styles.experience_year}>4</h1>
+                <h1 className={styles.experience_text}>Anos de Experiência</h1>
             </div>
             <div className={styles.about_content}>
                 <h2 className={styles.about_title}>Sobre Mim</h2>
-                <h3>Para além do meu papel como desenvolvedor, sou também um pai dedicado, um entusiasta de jogos e um apaixonado por esportes.
-                    Enquanto minha família é meu pilar e minha fonte de amor incondicional, encontrei na programação não apenas uma profissão, mas uma verdadeira vocação.
-                    Constantemente alinho minha carreira com meus interesses, buscando crescer tanto pessoal quanto profissionalmente a cada dia.
-                    Superar-me diariamente é meu objetivo, impulsionado por uma jornada de autodescoberta e aprimoramento constante, que me motiva profundamente.
+                <h3>Como desenvolvedor, equilibro minha vida com meu papel de pai dedicado, entusiasta de jogos e apaixonado por esportes.
+                    Minha família é meu suporte e minha fonte de amor, enquanto a programação se tornou mais do que uma profissão - é minha vocação.
+                    Busco constantemente integrar meus interesses à minha carreira, visando crescimento pessoal e profissional.
+                    Meu objetivo diário é superar-me, impulsionado por uma jornada de autodescoberta e aprimoramento constante, que me motiva profundamente.
                 </h3>
                 <div className={styles.complement}>
                     <div className={styles.first_complement_content}>
@@ -40,7 +62,7 @@ export default function About(props: IAboutProps) {
                     </div>
                 </div>
                 <div className={styles.button_container}>
-                    <PrimaryButton buttonFunction={() => {}} title='Carregar CV' />
+                    <PrimaryButton buttonFunction={() => handleDownload()} title='Carregar CV' />
                 </div>
             </div>
         </div>

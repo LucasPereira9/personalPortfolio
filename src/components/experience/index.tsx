ba3a542c70ab5c6e544ca254acd5c1510496b5ff
LucasPereira9 <lucas970997@gmail.com>
2024-05-15 09:30:35 -0300

    feat: title and subtitle added

---
+++ src/components/experience/index.tsx
@@ -0,0 +1,12 @@
+import styles from './experience.module.css'
+
+export default function Experience() {
+    return (
+        <div>
+            <div className={styles.title_container}>
+                <h3 className={styles.title}>Resumo</h3>
+                <h3 className={styles.subtitle}>Um pouco do meu trajeto</h3>
+            </div>
+        </div>
+    )
+}
